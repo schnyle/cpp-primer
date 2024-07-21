cf6183c96f0b5162ff457db1acf832a6cc083dbb
Kyle <kylesch115@gmail.com>
2024-07-20 18:22:14 -0600

    section 6.4

--- 6-functions/main.cpp
+++ 6-functions/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
-int &get(int *arr, int idx) { 
-    return arr[idx]; 
-}
+int calc(int, int);
+int calc(const int, const int);
+
+int get();
+double get();
+
+int *reset(int *);
+double *reset(double *);
 
 int main() {
-    int ia[10];
-    for (int i = 0; i != 10; ++i)
-        get(ia, i) = i;  // Assign the value of i to ia[i]
 
-    // Optional: Print the array to verify the values
-    for (int i = 0; i != 10; ++i)
-        std::cout << ia[i] << " ";
-    std::cout << std::endl;
-
-    return 0;
 }
